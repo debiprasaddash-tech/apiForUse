4d10cfa7cf4d66a9f393815f2b0d82b87e9f0a2b
debiprasad.dash_harns <debiprasad.dash@harnstech.com>
2023-08-31 10:31:45 +0530

    login added

--- controller/AllController.js
+++ controller/AllController.js
@@ -1,4 +1,7 @@
+const { default: axios } = require("axios");
 const { json } = require("body-parser");
+const { request } = require("express");
+const jwt = require("jsonwebtoken");
 
 const userData = [
   { username: "admin", password: "admin@123" },
@@ -22,8 +25,32 @@ exports.login = async (req, res) => {
       return res.status(400).json("password not match ! Try again");
     }
     docs = await userData.find((e) => e.username == req.body.username);
-    res.json(docs);
+    var token = jwt.sign(docs, "MyNamePrasant");
+    res.json(token);
   } catch (error) {
     res.status(500).json(error.message);
   }
 };
+
+exports.search = async (req, res) => {
+  try {
+    // verfy the user
+    let token = req.headers.authorization.split(" ")[1];
+    let verify = jwt.verify(token, "MyNamePrasant");
+
+    if (verify) {
+      let serachData = await axios.get(
+        `https://api.tvmaze.com/search/shows?q=${req.query}`
+      );
+      return res.json(serachData.data);
+    } else return res.json("Please provide correct details");
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
+
+// api example
+// get request
+// http://localhost:3000/api/search?q=girls
+// post request
+// http://localhost:3000/api/login
--- index.js
+++ index.js
@@ -3,11 +3,9 @@ const express = require("express");
 const allController = require("./controller/AllController.js");
 const app = express();
 const port = 3000;
-const router=express.Router()
+const router = express.Router();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.post("/api/login",allController.login);
-
-app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
-);
+app.post("/api/login", allController.login);
+app.get("/api/search", allController.search);
+app.listen(port, () => console.log(`http://localhost:${port}`));
